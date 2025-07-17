d78649be0a197ac492d643b6e7e454546efb6801
siddhik92 <siddhikawade92@gmail.com>
2025-07-17 12:14:06 +0530

    this is my dsa

---
+++ Sidhhi/DSA.java
@@ -0,0 +1,11 @@
+package Sidhhi;
+
+public class DSA {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 30;
+        int result = a + b;
+        System.out.println(result);
+    }
+
+}
