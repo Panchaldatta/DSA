2efd79f1d9ea351402dbc0889f7ec94b1a81427b
siddhik92 <siddhikawade92@gmail.com>
2025-07-17 11:59:16 +0530

    this is test

--- TEST/abc.java
+++ TEST/abc.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class abc{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.println("Hello");
         int n = sc.nextInt();
         int[] arr = new int[n];
         for(int i = 0; i < n; i++) {
